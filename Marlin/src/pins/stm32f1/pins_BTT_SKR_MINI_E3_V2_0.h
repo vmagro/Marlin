89a1c27213d805b58682c973f65c8b9bf17ed75f
Vinnie Magro <v@vinn.ie>
2021-03-07 14:31:50 -0800

    gardenc: change pin definitions to sub e0 for y2

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
@@ -63,7 +63,7 @@
   #define X_HARDWARE_SERIAL  MSerial4
   #define Y_HARDWARE_SERIAL  MSerial4
   #define Z_HARDWARE_SERIAL  MSerial4
-  #define E0_HARDWARE_SERIAL MSerial4
+  #define Y2_HARDWARE_SERIAL MSerial4
 
   // Default TMC slave addresses
   #ifndef X_SLAVE_ADDRESS
@@ -75,7 +75,7 @@
   #ifndef Z_SLAVE_ADDRESS
     #define Z_SLAVE_ADDRESS  1
   #endif
-  #ifndef E0_SLAVE_ADDRESS
-    #define E0_SLAVE_ADDRESS 3
+  #ifndef Y2_SLAVE_ADDRESS
+    #define Y2_SLAVE_ADDRESS 3
   #endif
 #endif
